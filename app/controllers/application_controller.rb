93c13a899d1e84466897e2e2c03601ed114b4794
ChinaHDJ <1035457356@qq.com>
2019-01-03 22:27:30 +0800

    fff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  def auth_token
+    result = App::UserToken.auth(token_record)
+    render json: { success: false, message: result.message } unless result.success
+  end
+
+  private
+
+  def token_record
+    params[:token]
+  end
+
 end
--- lib/app/token.rb
+++ lib/app/token.rb
@@ -1,16 +1,24 @@
 module App
-  class AppToken
+  class UserToken
     SECRET_KEY = ENV["SECRET_KEY"]
+    AbstractStruct = Struct.new(:success, :message)
+
+    ExpiredResult = AbstractStruct.new(false, "用户会话已过期，请重新登入")
+    NotBeforeTimerResult = AbstractStruct.new(false, "token 无法验证")
 
     class << self
+      def auth(token)
+        begin
+          json = JWT.decode token, SECRET_KEY, true, { algorithm: 'HS256' }
 
-      def encode(payload)
-        raise if payload.blank?
-
-        JWT.encode payload, SECRET_KEY, 'HS256'
+        rescue JWT::ExpiredSignature
+          return ExpiredResult
+        rescue JWT::JWT::ImmatureSignature
+          return NotBeforeTimerResult
+        end
       end
 
-      def decode(token)
+      def create(token)
         raise if token.blank?
 
         JWT.decode token, SECRET_KEY, true, { algorithm: 'HS256' }
